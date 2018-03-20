e6881f95a8c8f74c174a98b8753ff884e8af34b2
PanteoPro <panteopro@gmail.com>
2018-03-20 20:17:11 +0300

    initial commit

--- Canvas_Properties/js/main.js
+++ Canvas_Properties/js/main.js
@@ -265,4 +265,88 @@ window.onload = function(){
 			ctx.fill();
 		}
 	}
+
+	var canvas1 = document.querySelector("#canvas11_1");
+	var canvas2 = document.querySelector("#canvas11_2");
+	if(canvas1 && canvas1.getContext){
+		var ctx1 = canvas1.getContext("2d");
+		var ctx2 = canvas2.getContext("2d");
+		if(ctx1){
+			var img = new Image();
+
+			ctx1.strokeStyle = "#213125";
+			ctx1.lineWidth = 10;
+			ctx1.arc(canvas1.width/2,canvas1.height/2,100,0,Math.PI*2);
+			ctx1.stroke();
+			ctx1.clip();
+
+			ctx2.beginPath();
+			ctx2.moveTo(10,canvas2.height/2);
+			ctx2.lineTo(50,10);
+			ctx2.lineTo(180, 180);
+			ctx2.lineTo(30, 220);
+			ctx2.closePath();
+			ctx2.clip();
+
+			img.onload = function(){
+				ctx1.drawImage(img,0,100,250,250,0,0,250,250);
+				ctx2.drawImage(img,0,100,250,250,0,0,250,250);
+			}
+			img.src = "img/sprites2.png";
+		}
+	}
+
+	var canvas = document.querySelector("#canvas12");
+	if(canvas && canvas.getContext){
+		var ctx = canvas.getContext("2d");
+		if(ctx){
+			ctx.fillStyle = "blue";
+			ctx.save();
+			ctx.fillRect(0,0,120,40);
+
+			ctx.translate(0,60);
+			ctx.fillRect(0,0,120,40);
+			ctx.restore();
+			ctx.save();
+
+			ctx.fillStyle = "red";
+			ctx.translate(140,0);
+			ctx.fillRect(0,0,120,40);
+			ctx.translate(0,60);
+			ctx.scale(1.5,1.5);
+			ctx.fillRect(0,0,120,40);
+			ctx.restore();
+			ctx.save();
+
+			var radian = (Math.PI / 180) * 20;
+			ctx.translate(60,160);
+			for(var degrees = 0; degrees < 360; degrees += 20){
+				ctx.rotate(radian);
+				ctx.beginPath();
+				ctx.moveTo(-50,0);
+				ctx.lineTo(50,0);
+				ctx.stroke();
+			}
+			
+			ctx.restore();
+			ctx.fillStyle = "green";
+			var sx = 0.2;
+			var sy = 0;
+			ctx.setTransform(1,sy,sx,1,0,0);
+			ctx.fillRect(380,120,30,30);
+
+		}
+	}
+
+	var canvas = document.querySelector("#canvas13");
+	if(canvas && canvas.getContext){
+		var ctx = canvas.getContext("2d");
+		if(ctx){
+			var img = new Image();
+			img.onload = function(){
+				ctx.drawImage(img,0,0);
+			}
+			img.src = "img/img.png";
+		}
+	}
 }
